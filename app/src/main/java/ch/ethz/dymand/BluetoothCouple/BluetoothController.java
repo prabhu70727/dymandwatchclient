2b21e6b77cbcb90f5bd5996f1c964c2aafa0820b
prabhu70727 <psanthan@student.ethz.ch>
2018-09-04 13:05:56 +0200

    Adding the BluetoothController.java

--- app/src/main/java/ch/ethz/dymand/BluetoothCouple/BluetoothController.java
+++ app/src/main/java/ch/ethz/dymand/BluetoothCouple/BluetoothController.java
@@ -6,22 +6,34 @@ import android.util.Log;
 
 import java.io.IOException;
 
+import ch.ethz.dymand.Callbacks;
 import ch.ethz.dymand.Config;
+import ch.ethz.dymand.MainActivity;
+import ch.ethz.dymand.VoiceActivityDetection.VAD;
 
 
 public class BluetoothController implements
         BluetoothCentralConnect.CentralConnectInterface,
         BluetoothCentralScan.CentralScanInterface,
-        BluetoothPeripheral.PeripheralInterface {
+        BluetoothPeripheral.PeripheralInterface,
+        Callbacks.BleCallback, VAD.DataCollectionListener {
 
+    public static BluetoothController bluetoothController = null;
     BluetoothCentralScan mBluetoothCentralScan;
     BluetoothPeripheral mBluetoothPeripheral;
     BluetoothCentralConnect mBluetoothCentralConnect;
+    BluetoothDevice mDevice;
 
-    private final Context mContext;
+    private Context mContext;
+    private VAD voiceDetector;
 
-    public BluetoothController(Context context) {
-        mContext = context;
+    public BluetoothController getInstance(Context context) {
+        if(bluetoothController!=null) return bluetoothController;
+        else {
+            bluetoothController = new BluetoothController();
+            bluetoothController.mContext = context;
+            return bluetoothController;
+        }
     }
 
     private static final String LOG_TAG = "Logs: BluetoothController";
@@ -39,43 +51,31 @@ public class BluetoothController implements
     }
 
     // Central device
+    // todo: george.. callback for data collection
     @Override
     public void connected() {
         Log.i(LOG_TAG, "Central Connected");
         Log.i(LOG_TAG, "Start recording");
+        // Code to start recording..
 
-
-        // --- start of Example code to stop scanning
-        try {
-            Thread.sleep(5000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-        try {
-            mBluetoothCentralScan.stopScan();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        // --- end of Example code to stop scanning
-
+        // After recording
     }
 
     // Central device
     @Override
     public void notConnected(BluetoothDevice device) {
         Log.i(LOG_TAG, "Not connected");
-        // maybe retry connection... or just scanning part?
+        // maybe retry connection... or scanning part?
+        Config.shouldConnect = true;
     }
 
     // Central device
-
     @Override
     public void found(BluetoothDevice device) {
-        // I am skipping the code to try for VAD... after VAD returns true, we use the below code for it's callback and not here
-        // The below code should be put in VAD's return
-        Log.i(LOG_TAG, "Device found trying to connect (also sending a timestamp message)");
-        mBluetoothCentralConnect = new BluetoothCentralConnect(mContext, this);
-        mBluetoothCentralConnect.connectDevice(device,System.currentTimeMillis()+"");
+        Log.i(LOG_TAG, "Device found trying for VAD");
+        voiceDetector = new VAD(this);
+        voiceDetector.recordSound();
+        mDevice = device;
     }
 
     // peripheral device
@@ -85,15 +85,98 @@ public class BluetoothController implements
         Log.i(LOG_TAG, "Start recording");
         // Code to start recording..
         // After recording
+    }
 
-        //-- start Example code to stop advertising
-        try {
-            Thread.sleep(5000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
+    @Override
+    public void speech() {
+        if ((mDevice == null)) throw new AssertionError();
+        Log.i(LOG_TAG, "Voice detected trying to connect (also sending a timestamp message)");
+        mBluetoothCentralConnect = new BluetoothCentralConnect(mContext, this);
+        mBluetoothCentralConnect.connectDevice(mDevice,System.currentTimeMillis()+"");
+        //mDevice = null;
+    }
+
+    //todo: prabhu: Logically it works, but still it is better if we can restart the scanning/adv process.
+    @Override
+    public void noSpeech() {
+        Config.shouldConnect = true;
+    }
+
+
+    @Override
+    public void startBleCallback()  {
+        if ((!(Config.shouldConnect == true))) throw new AssertionError();
+
+        if(Config.isCentral) {
+            if(mBluetoothCentralScan!=null){
+                try {
+                    mBluetoothCentralScan.stopScan();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            mBluetoothCentralScan = new BluetoothCentralScan(mContext, this);
+            try {
+                mBluetoothCentralScan.startScan();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
-        mBluetoothPeripheral.stopAdvertising();
-        // -- end Example code to stop advertising
+        else {
+            if(mBluetoothPeripheral!=null){
+                mBluetoothPeripheral.stopAdvertising();
+            }
+            mBluetoothPeripheral = new BluetoothPeripheral(mContext, this);
+            mBluetoothPeripheral.startAdvertising();
+        }
+    }
 
+    @Override
+    public void stopBleCallback() {
+        if(Config.isCentral) {
+            if(mBluetoothCentralScan!=null){
+                try {
+                    mBluetoothCentralScan.stopScan();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            mBluetoothCentralScan = null;
+        }
+        else {
+            if(mBluetoothPeripheral!=null){
+                mBluetoothPeripheral.stopAdvertising();
+            }
+            mBluetoothPeripheral = null;
+        }
+
+    }
+
+    @Override
+    public void reStartBleCallback() {
+        if(Config.isCentral) {
+            if(mBluetoothCentralScan!=null){
+                try {
+                    mBluetoothCentralScan.stopScan();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            mBluetoothCentralScan = new BluetoothCentralScan(mContext, this);
+            try {
+                mBluetoothCentralScan.startScan();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        else {
+            if(mBluetoothPeripheral!=null){
+                mBluetoothPeripheral.stopAdvertising();
+            }
+            mBluetoothPeripheral = new BluetoothPeripheral(mContext, this);
+            mBluetoothPeripheral.startAdvertising();
+        }
     }
 }
